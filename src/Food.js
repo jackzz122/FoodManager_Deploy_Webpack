cafec7a6541b86e0555f46b776abc535233d63c8
jackzz122 <vuongducluong0369@gmail.com>
2025-02-13 19:46:46 +0700

    Fix image webpack ver3

--- src/Food.js
+++ src/Food.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import React from "react";
-const images = require.context("./assets", false, /\.(jpeg|jpg|png)$/);
 export default function Food(props) {
   return (
     <div className="food_items bg-white shadow-md">
-      <img src={require(`${props.img}`).default} className="pic" />
+      <img src={props.img} className="pic" alt={props.title} />
       <div className="p-6">
         <div className="infor_food flex justify-between items-center ">
           <p className="text-xl capitalize ">{props.title}</p>
